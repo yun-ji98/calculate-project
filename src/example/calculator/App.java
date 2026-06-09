854f5e63269ac7939739e9a84bbe6e4772f418ed
yun-ji98 <lyj77731@naver.com>
2026-06-09 14:48:19 +0900

    사칙연산 나눗셈까지

--- src/example/calculator/App.java
+++ src/example/calculator/App.java
@@ -19,14 +19,32 @@ public class App {
 
         //사칙연산 기호 입력받기
         System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.next().charAt(0);
+        String operator = sc.next();
 
         //위에서 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력하기
         int result = 0;
 
-        if (operator=='+')
+        if (operator.equals("+")) {
             result = firstNumber + secondNumber;
-        System.out.println("result = " + result);
+            System.out.println("result = " + result);
+
+        } else if (operator.equals("-")) {
+            result = firstNumber - secondNumber;
+            System.out.println("result = " + result);
+
+        } else if (operator.equals("*")) {
+            result = firstNumber * secondNumber;
+            System.out.println("result = " + result);
+
+        } else if (operator.equals("/")) {
+            if (secondNumber == 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            } else {
+                result = firstNumber / secondNumber;
+                System.out.println("result: " + result);
+            }
+
+        }
 
 
 
