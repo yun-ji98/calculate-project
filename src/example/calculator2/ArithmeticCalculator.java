62117ea50678e65c20d4d00ffa7c5977fcdf6346
yun-ji98 <lyj77731@naver.com>
2026-06-19 12:31:40 +0900

    enum2

--- src/example/calculator2/ArithmeticCalculator.java
+++ src/example/calculator2/ArithmeticCalculator.java
@@ -2,21 +2,37 @@ package example.calculator2;
 
 public class ArithmeticCalculator {
     public static void main(String[] args) {
-        enum OperatorType {
-            plus('+'),
-            minus('-'),
-            multiply('*'),
-            divide('/');
 
-           private final char symbol;
+    }
 
-           OperatorType(char symbol) {
-               this.symbol = symbol;
+    enum OperatorType {
+        PLUS('+'),
+        MINUS('-'),
+        MULTIPLY('*'),
+        DIVIDE('/');
+
+
+        private final char symbol;
+
+        OperatorType(char symbol) {
+            this.symbol = symbol;
+        }
+
+        public char getSymbol() {
+            return symbol;
+        }
+
+        public static OperatorType fromSymbol ( char operator) {
+            OperatorType[] values = OperatorType.values();
+            for (int i = 0; i < values.length; i++) {
+                OperatorType value = values[i];
+
+                if (value.getSymbol() == operator) {
+                    return value;
+                }
             }
 
-            public char getSymbol() {
-               return symbol;
-            }
+            throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
         }
     }
 }
